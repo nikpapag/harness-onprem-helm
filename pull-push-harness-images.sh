25ba510d942c095783d039495b0281d32b472e22
nikpapag <82569427+nikpapag@users.noreply.github.com>
2022-09-07 15:15:57 +0100

    Create pull-push-harness-images.sh

---
+++ pull-push-harness-images.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Change to the cloned repo path of the chart
+helm_chart_path="/Users/nikp/socgen/onprem-7-sep/helm-charts/src/demo/"
+#File to store the image list
+images_file="harness-images.txt"
+#Private repository url
+repository="nikpnexus.co.uk:7777/repository/harness/"
+
+
+#Helm template to extract all the images used in the chart
+helm template $helm_chart_path | grep "image:" | tr -d "[:blank:]" | sed 's/image://g' > $images_file
+
+#Parse images file
+#Pull images from public repos
+#Tag image according to your private repo
+#Push image to the registry
+#Missing the authentication to the private registry
+
+while IFS= read -r line
+do
+  echo "$line"
+  docker pull "$line"
+  image=$(echo $line |sed 's/docker.io\///g')
+  docker tag $image "$repository$image"
+  docker push "$repository$image"
+done < "$images_file"
