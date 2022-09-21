37eed896855af29e6e8f43e49b550783f3df4bcd
nikpapag <82569427+nikpapag@users.noreply.github.com>
2022-09-21 15:21:14 +0100

    Update pull-push-harness-images.sh

--- pull-push-harness-images.sh
+++ pull-push-harness-images.sh
@@ -22,6 +22,7 @@ do
   echo "$line"
   docker pull "$line"
   image=$(echo $line |sed 's/docker.io\///g')
+  image=$(echo $line |sed 's/docker.io\///g' | sed 's/harness\///g' | sed 's/curl\///g'| sed 's/bitnami\///g' )
   docker tag $image "$repository$image"
   docker push "$repository$image"
 done < "$images_file"
